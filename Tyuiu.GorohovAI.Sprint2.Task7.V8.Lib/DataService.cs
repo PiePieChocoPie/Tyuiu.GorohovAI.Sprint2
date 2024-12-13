e357f1426090a04d2b1e5de54d2c12aa1e2a7273
Александр Горохов <39surara@gmail.com>
2024-12-13 21:55:10 +0500

    213213

---
+++ Tyuiu.GorohovAI.Sprint2.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.GorohovAI.Sprint2.Task7.V8.Lib
+{
+    public class DataService : ISprint2Task7V8
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool isInside = (x <= 1 && y <= Math.Pow(x, 2)) || (x > 1 && y <= 4);
+            return isInside;
+        }
+    }
+}
