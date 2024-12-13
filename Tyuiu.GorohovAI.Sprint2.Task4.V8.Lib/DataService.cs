9b7da70133ab2b4b097d9c497f757f1c00f51077
Александр Горохов <39surara@gmail.com>
2024-12-13 18:37:45 +0500

    21321

---
+++ Tyuiu.GorohovAI.Sprint2.Task4.V8.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.GorohovAI.Sprint2.Task4.V8.Lib
+{
+    public class DataService : ISprint2Task4V8
+    {
+        public double Calculate(double x, double y)
+        {
+
+            double z = 0;
+            if (x * 2 > y + 15)
+            {
+                z = Math.Pow((5 + (Math.Pow(y + 5, 1 / 2) / Math.Pow(x, 3))), x);
+            }
+            else
+            {
+                z = 2 * x * x + 2 * x - (4 + 6 / y * y);
+            }
+            return z;
+        }
+    }
+}
---
+++ Tyuiu.GorohovAI.Sprint2.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GorohovAI.Sprint2.Task4.V8.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
