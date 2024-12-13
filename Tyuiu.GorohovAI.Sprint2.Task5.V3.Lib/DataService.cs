bde3b2c36b9e95289af2e9da866dcb9a99ea88c9
Александр Горохов <39surara@gmail.com>
2024-12-13 21:41:14 +0500

    21312

---
+++ Tyuiu.GorohovAI.Sprint2.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.GorohovAI.Sprint2.Task5.V3.Lib
+{
+    public class DataService : ISprint2Task5V3
+    {
+        public string FindDayName(int dayNumber)
+        {
+            string dayName = "";
+            switch (dayNumber)
+            {
+                case 1:
+                    dayName = "Понедельник";
+                    break;
+                case 2:
+                    dayName = "Вторник";
+                    break;
+                case 3:
+                    dayName = "Среда";
+                    break;
+                case 4:
+                    dayName = "Четверг";
+                    break;
+                case 5:
+                    dayName = "Пятница";
+                    break;
+                case 6:
+                    dayName = "Суббота";
+                    break;
+                case 7:
+                    dayName = "Воскресенье";
+                    break;
+                default:
+                    dayName = "Некорректный номер дня недели. Введите число от 1 до 7.";
+                    break;
+            }
+            return dayName;
+        }
+    }
+}
