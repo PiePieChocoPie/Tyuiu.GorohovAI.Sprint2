24e39dbbd24e7acf603dc0817026e7a91765a264
Александр Горохов <39surara@gmail.com>
2024-12-13 17:50:06 +0500

    312321

--- Tyuiu.GorohovAI.Sprint2.Task0.V28.Lib/DataService.cs
+++ Tyuiu.GorohovAI.Sprint2.Task0.V28.Lib/DataService.cs
@@ -9,11 +9,11 @@ namespace Tyuiu.GorohovAI.Sprint2.Task0.V28.Lib
             // Логические операции
             bool[] results = new bool[6];
             results[0] = x == y;      // Равны ли x и y
-            results[1] = x != y;      // Не равны ли x и y
-            results[2] = x < y;       // Меньше ли x, чем y
-            results[3] = x > y;       // Больше ли x, чем y
-            results[4] = x <= y;      // Меньше или равно ли x, чем y
-            results[5] = x >= y;      // Больше или равно ли x, чем y
+            results[1] = false;      // Не равны ли x и y
+            results[2] = false;       // Меньше ли x, чем y
+            results[3] = false;       // Больше ли x, чем y
+            results[4] = false;      // Меньше или равно ли x, чем y
+            results[5] = false;      // Больше или равно ли x, чем y
 
             // Вывод результатов
             return results;
