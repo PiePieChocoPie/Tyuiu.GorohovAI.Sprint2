8d99462250aec4051e5fce7fd95f5d9b4658737b
Александр Горохов <39surara@gmail.com>
2024-12-13 21:50:10 +0500

    321321

---
+++ Tyuiu.GorohovAI.Sprint2.Task6.V1.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.GorohovAI.Sprint2.Task6.V1.Lib
+{
+    public class DataService : ISprint2Task6V1
+    {
+        public int FindMonthDaysCount(int value)
+        {
+            int daysInMonth = value switch
+            {
+                1 or 3 or 5 or 7 or 8 or 10 or 12 => 31, // Январь, Март, Май, Июль, Август, Октябрь, Декабрь
+                4 or 6 or 9 or 11 => 30,              // Апрель, Июнь, Сентябрь, Ноябрь
+                2 => 28,                              // Февраль (без учёта високосного года)
+                _ => -1                               // Некорректный ввод
+            };
+
+            if (daysInMonth != -1)
+            {
+                Console.WriteLine($"В месяце {value} дней: {daysInMonth}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: номер месяца должен быть в диапазоне от 1 до 12.");
+            }
+            return daysInMonth;
+        }
+    }
+}
---
+++ Tyuiu.GorohovAI.Sprint2.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GorohovAI.Sprint2.Task6.V1.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
